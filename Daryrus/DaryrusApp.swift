9032ec5dfc7bb0273f2b60c23ec2b2c36aca9427
Lee Jon Chapman <leejonchapman@icloud.com>
2024-12-07 13:43:22 +0000

    Initial Commit

---
+++ Daryrus/DaryrusApp.swift
@@ -0,0 +1,17 @@
+//
+//  DaryrusApp.swift
+//  Daryrus
+//
+//  Created by Lee Chapman on 07/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct DaryrusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DaryrusTests/DaryrusTests.swift
@@ -0,0 +1,17 @@
+//
+//  DaryrusTests.swift
+//  DaryrusTests
+//
+//  Created by Lee Chapman on 07/12/2024.
+//
+
+import Testing
+@testable import Daryrus
+
+struct DaryrusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
